4ae62dd60de06964370e5c1075c40b83d772a402
Linda0811 <langegger.linda@icloud.com>
2019-01-28 09:32:08 +0100

    beginning SceneSwitcher

---
+++ ZahnfeeV2/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneSwitcher : MonoBehaviour {
+
+    enum scenes { Start, Main, End };
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene((init)scenes.Start);
+    }
+
+    public void LoadMainScene()
+    {
+        SceneManager.LoadScene((init)scenes.Main)
+    }
+
+    public void LoadEndScene()
+    {
+        SceneManager.LoadScene((init)scenes.End)
+    }
+
+    public void QuitGame ()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+
+}
